4423bf0d5bf754a132c99e2723dcada72f93641e
Mahery Gonin <mahery.gonin@gmail.com>
2021-07-12 11:55:19 +0200

    Update models

--- app_projects/models.py
+++ app_projects/models.py
@@ -1,29 +1,44 @@
 from django.db import models
 from django.conf import settings
 
+PRIORITY_CHOICES = [
+    ('FAIBLE', 'FAIBLE'),
+    ('MOYENNE', 'MOYENNE'),
+    ('ELEVEE', 'ELEVEE'),
+]
+STATS_CHOICES = [
+    ('A FAIRE', 'A FAIRE'),
+    ('EN COURS', 'EN COURS'),
+    ('TERMINE', 'TERMINE'),
+]
+TAG_CHOICES = [
+    ('BUG', 'BUG'),
+    ('AMELIORATION', 'AMELIORATION'),
+    ('TACHE', 'TACHE'),
+]
 
 class Contributors(models.Model):
-    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
     project_id = models.IntegerField()
     permission = models.Choices()
-    role = models.CharField(max_length=128, blank=False)
+    role = models.CharField(max_length=128)
 
 
 class Projects(models.Model):
     project_id = models.IntegerField()
-    title = models.CharField(max_length=128, blank=False)
-    description = models.TextField(max_length=2048, blank=False)
-    type = models.CharField(max_length=128, blank=False)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+    type = models.CharField(max_length=128)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Issues(models.Model):
-    title = models.CharField(max_length=128, blank=False)
-    desc = models.TextField(max_length=2048, blank=False)
-    tag = models.CharField(max_length=128, blank=False)
-    priority = models.CharField(max_length=128, blank=False)
-    project_id = models.ForeignKey()
-    status = models.CharField(max_length=128, blank=False)
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=2048)
+    tag = models.CharField(choices=TAG_CHOICES, max_length=100)
+    priority = models.CharField(choices=PRIORITY_CHOICES, default='MOYENNE', max_length=100)
+    project_id = models.IntegerField()
+    status = models.CharField(choices=STATS_CHOICES, default='A FAIRE', max_length=100)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL)
     created_time = models.DateTimeField(auto_now_add=True)
@@ -31,7 +46,7 @@ class Issues(models.Model):
 
 class Comments(models.Model):
     comment_id = models.IntegerField()
-    description = models.TextField(max_length=2048)
+    description = models.CharField(max_length=2048)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    issue_id = models.IntegerField()
+    issue_id = models.ForeignKey()
     created_time = models.DateTimeField(auto_now_add=True)
