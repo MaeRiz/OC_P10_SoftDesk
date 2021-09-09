d841287447618478c4da9ef0e421438f4fc6b7c8
Mahery Gonin <mahery.gonin@gmail.com>
2021-09-09 06:29:43 +0200

    Add JWT authentification

--- softdesk/app_auth/views.py
+++ softdesk/app_auth/views.py
@@ -1,16 +1,10 @@
 from django.contrib.auth.models import User
-from .serializers import SignupSerializer, MyTokenObtainPairSerializer
+from .serializers import SignupSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = SignupSerializer
-
-
-class MyObtainTokenPairView(TokenObtainPairView):
-    permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
