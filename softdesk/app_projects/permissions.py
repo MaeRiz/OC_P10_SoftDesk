e46dd0b416626888dfe084911a26743d6a334240
Mahery Gonin <mahery.gonin@gmail.com>
2021-09-22 22:06:38 +0200

    Fixed permissions

--- softdesk/app_projects/permissions.py
+++ softdesk/app_projects/permissions.py
@@ -17,20 +17,29 @@ class ContributorPermissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
         project = Project.objects.get(id=view.kwargs['project_pk'])
+        usr_projects = Project.objects.filter(contributors__user=request.user)
+        if project in usr_projects:
+            project = Project.objects.get(id=view.kwargs['project_pk'])
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            return request.user == project.author
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        project = Project.objects.get(id=view.kwargs['project_pk'])
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user == project.author
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user == obj.author
 
 
 class IssuePermissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
+        project = Project.objects.get(id=view.kwargs['project_pk'])
+        usr_projects = Project.objects.filter(contributors__user=request.user)
+        if project in usr_projects:
+            return True
+        return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
@@ -41,7 +50,11 @@ class IssuePermissions(permissions.BasePermission):
 class CommentPermissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
+        project = Project.objects.get(id=view.kwargs['project_pk'])
+        usr_projects = Project.objects.filter(contributors__user=request.user)
+        if project in usr_projects:
+            return True
+        return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
